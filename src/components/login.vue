b47569f6620d169fa3b7b17ebe02c8f47f0ad67b
shawxy <1146717626@qq.com>
2019-08-22 19:45:21 +0800

    登录页面

---
+++ src/components/login.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="lg-contain">
+  <el-card class="card">
+    <img src="../assets/images/logo_index.png" alt="">
+    <el-form :model="loginForm">
+      <el-form-item>
+        <el-input v-model="loginForm.mobile" placeholder="请输入您的手机号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:335px;margin-right:10px"></el-input>
+      <el-button>发送验证码</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="width:100%">登录</el-button>
+      </el-form-item>
+
+    </el-form>
+  </el-card>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.lg-contain{
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background: url('../assets/images/login_bg.jpg') center / cover;
+  .card{
+    width: 500px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    img{
+      display: block;
+      width: auto;
+      margin: 20px auto 30px;
+    }
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Login from '../components/login.vue'
+
+Vue.use(VueRouter)
+export default new VueRouter({
+  routes: [
+    { path: '/login', name: 'login', component: Login }
+  ]
+})
